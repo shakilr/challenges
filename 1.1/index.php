55db16cfa28b9b28a75753e2e907e8a453c6e6f1
dev <dev@example.com>
2020-09-17 19:02:59 +0500

    init

---
+++ 1.1/index.php
@@ -0,0 +1,22 @@
+<?php
+$array =  [2, 1, 4, 5, 2, 7, 4];
+
+function twosum($array, $sum = 0 ) {
+
+    $new_array = [];
+
+    for( $i = 0 ; $i < count($array); $i++ ) {
+
+        for( $j = $i + 1; $j < count($array); $j++ ) {
+            if( $array[$i] + $array[$j] == $sum )  {
+                $array =  [ $i, $j ];
+            }
+        }
+    }
+
+   return $array;
+}
+
+echo '<pre>';
+    print_r(twosum($array, 7));
+echo '</pre>';
